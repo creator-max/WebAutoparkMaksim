21fda98dd691d895655f91a87cfb233c259f9402
ASUS <maksim.maksimeika@gmail.com>
2021-12-13 23:40:07 +0300

    Add Repositories for all tables.

---
+++ WebAutopark.DataAccesLayer/Repositories/OrderRepository.cs
@@ -0,0 +1,36 @@
+﻿using Dapper;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebAutopark.DataAccesLayer.Entities;
+using WebAutopark.DataAccesLayer.Interfaces;
+
+namespace WebAutopark.DataAccesLayer.Repositories
+{
+    public class OrderRepository : BaseRepository, IRepository<Order>
+    {
+        private const string QueryGetAll = "SELECT * FROM Orders";
+
+        private const string QueryGetById = "SELECT * FROM Orders " +
+                                            "WHERE OrderId = @OrderId";
+
+        private const string QueryCreate = "INSERT INTO Orders(VehicleId) " +
+                                            "VALUES(@VehicleId)";
+
+        private const string QueryDelete = "DELETE FROM Orders" +
+                                           "WHERE OrderId = @OrderId";
+
+        private const string QueryUpdate = "UPDATE Orders SET" +
+                                           "VehicleId = @VehicleId" +
+                                           "WHERE OrderId = @OrderId";
+
+        public OrderRepository(IConnectionStringProvider connectionStringProvider)
+            : base(connectionStringProvider)
+        { }
+
+        public async Task Create(Order item) => await Connection.ExecuteAsync(QueryCreate, item);
+        public async Task Delete(int id) => await Connection.ExecuteAsync(QueryDelete, id);
+        public async Task<IEnumerable<Order>> GetAll() => await Connection.QueryAsync<Order>(QueryGetAll);
+        public async Task<Order> GetById(int id) => await Connection.QueryFirstOrDefaultAsync<Order>(QueryGetById, id);
+        public async Task Update(Order item) => await Connection.ExecuteAsync(QueryUpdate, item);
+    }
+}
