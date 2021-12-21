9f474921ee89137eea6d05b1fa8fede8ae3ac6d1
ASUS <maksim.maksimeika@gmail.com>
2021-12-21 18:48:06 +0300

    Refactor code,

--- WebAutopark.DataAccesLayer/Repositories/VehicleRepository.cs
+++ WebAutopark.DataAccesLayer/Repositories/VehicleRepository.cs
@@ -8,41 +8,50 @@ namespace WebAutopark.DataAccesLayer.Repositories
 {
     public class VehicleRepository : BaseRepository, IRepository<Vehicle>
     {
-        private const string QueryGetAll = "SELECT * FROM Vehicles";
+        private const string QueryGetAll = "SELECT * FROM Vehicles ";
 
         private const string QueryGetById = "SELECT * FROM Vehicles " +
-                                             "WHERE VehicleId = @VehicleId";
+                                             "WHERE VehicleId = @VehicleId ";
 
-        private const string QueryCreate = "INSERT INTO Vehicles (VehicleTypeId, Model," +
-                                            "YearOfIssue, Weight, TankCapacity, LicensePlat" +
+        private const string QueryCreate = "INSERT INTO Vehicles (VehicleTypeId, Model, " +
+                                            "YearOfIssue, Weight, TankCapacity, LicensePlat " +
                                             "Mileage, Color) " +
-                                            "VALUES(@VehicleTypeId, @Model, @YearOfIssue," +
+                                            "VALUES(@VehicleTypeId, @Model, @YearOfIssue, " +
                                             "@Weight, @TankCapacity, @LicensePlat, " +
                                             "@Mileage, @Color)";
 
-        private const string QueryDelete = "DELETE FROM Vehicles" +
-                                            "WHERE VehicleId = @VehicleId";
+        private const string QueryDelete = "DELETE FROM Vehicles " +
+                                            "WHERE VehicleId = @VehicleId ";
 
-        private const string QueryUpdate = "UPDATE Vehicles SET" +
-                                            "VehicleTypeId = @VehicleTypeId," +
-                                            "Model         = @Model," +
-                                            "YearOfIssue   = @YearOfIssue," +
-                                            "Weight        = @Weight," +
-                                            "TankCapacity  = @TankCapacity," +
-                                            "LicensePlat   = @LicensePlat," +
+        private const string QueryUpdate = "UPDATE Vehicles SET " +
+                                            "VehicleTypeId = @VehicleTypeId, " +
+                                            "Model         = @Model, " +
+                                            "YearOfIssue   = @YearOfIssue, " +
+                                            "Weight        = @Weight, " +
+                                            "TankCapacity  = @TankCapacity, " +
+                                            "LicensePlat   = @LicensePlat, " +
                                             "Mileage       = @Mileage, " +
-                                            "Color         = @Color" +
-                                            "WHERE VehicleId = @VehicleId";
+                                            "Color         = @Color " +
+                                            "WHERE VehicleId = @VehicleId ";
 
         public VehicleRepository(IConnectionStringProvider connectionStringProvider) :
             base(connectionStringProvider)
         { }
 
-        public async Task Create(Vehicle item) => await Connection.ExecuteAsync(QueryCreate, item);
-        public async Task Delete(int id) => await Connection.ExecuteAsync(QueryDelete, id);
-        public async Task<IEnumerable<Vehicle>> GetAll() => await Connection.QueryAsync<Vehicle>(QueryGetAll);
-        public async Task<Vehicle> GetById(int id) => await Connection.QueryFirstOrDefaultAsync<Vehicle>(QueryGetById, id);
-        public async Task Update(Vehicle item) => await Connection.ExecuteAsync(QueryUpdate, item);
+        public async Task Create(Vehicle item)
+            => await Connection.ExecuteAsync(QueryCreate, item);
+
+        public async Task Delete(int id) 
+            => await Connection.ExecuteAsync(QueryDelete, new { VehicleId = id});
+
+        public async Task<IEnumerable<Vehicle>> GetAll() 
+            => await Connection.QueryAsync<Vehicle>(QueryGetAll);
+
+        public async Task<Vehicle> GetById(int id)
+            => await Connection.QueryFirstOrDefaultAsync<Vehicle>(QueryGetById, new { VehicleId = id });
+
+        public async Task Update(Vehicle item) 
+            => await Connection.ExecuteAsync(QueryUpdate, item);
 
     }
 }
--- WebAutopark.DataAccesLayer/Repositories/VehicleTypeRepository.cs
+++ WebAutopark.DataAccesLayer/Repositories/VehicleTypeRepository.cs
@@ -8,30 +8,39 @@ namespace WebAutopark.DataAccesLayer.Repositories
 {
     public class VehicleTypeRepository : BaseRepository, IRepository<VehicleType>
     {
-        private const string QueryGetAll = "SELECT * FROM VehicleTypes";
+        private const string QueryGetAll = "SELECT * FROM VehicleTypes ";
 
         private const string QueryGetById = "SELECT * FROM VehicleTypes " +
-                                             "WHERE TypeId = @VehicleTypeId";
+                                             "WHERE TypeId = @VehicleTypeId ";
 
-        private const string QueryCreate = "INSERT INTO VehicleTypes (TypeName, TaxCoefficient)" +
+        private const string QueryCreate = "INSERT INTO VehicleTypes (TypeName, TaxCoefficient) " +
                                             "VALUES(@VehicleTypeName, @TaxCoefficient)";
 
-        private const string QueryDelete = "DELETE FROM VehicleTypes" +
+        private const string QueryDelete = "DELETE FROM VehicleTypes " +
                                             "WHERE TypeId = @VehicleTypeId";
 
-        private const string QueryUpdate = "UPDATE VehicleTypes SET" +
-                                            "TypeName       = @VehicleId," +
-                                            "TaxCoefficient = @TaxCoefficient" +
-                                            "WHERE TypeId = @VehicleTypeId";
+        private const string QueryUpdate = "UPDATE VehicleTypes SET " +
+                                            "TypeName       = @VehicleId, " +
+                                            "TaxCoefficient = @TaxCoefficient " +
+                                            "WHERE TypeId = @VehicleTypeId ";
 
         public VehicleTypeRepository(IConnectionStringProvider connectionStringProvider) :
             base(connectionStringProvider)
         { }
 
-        public async Task Create(VehicleType item) => await Connection.ExecuteAsync(QueryCreate, item);
-        public async Task Delete(int id) => await Connection.ExecuteAsync(QueryDelete, id);
-        public async Task<IEnumerable<VehicleType>> GetAll() => await Connection.QueryAsync<VehicleType>(QueryGetAll);
-        public async Task<VehicleType> GetById(int id) => await Connection.QueryFirstOrDefaultAsync<VehicleType>(QueryGetById, id);
-        public async Task Update(VehicleType item) => await Connection.ExecuteAsync(QueryUpdate, item);
+        public async Task Create(VehicleType item)
+            => await Connection.ExecuteAsync(QueryCreate, item);
+
+        public async Task Delete(int id)
+            => await Connection.ExecuteAsync(QueryDelete, new { VehicleTypeId = id });
+
+        public async Task<IEnumerable<VehicleType>> GetAll()
+            => await Connection.QueryAsync<VehicleType>(QueryGetAll);
+
+        public async Task<VehicleType> GetById(int id)
+            => await Connection.QueryFirstOrDefaultAsync<VehicleType>(QueryGetById, new { VehicleTypeId = id });
+
+        public async Task Update(VehicleType item) 
+            => await Connection.ExecuteAsync(QueryUpdate, item);
     }
 }
